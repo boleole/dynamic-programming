f00e5ed7336f3e2bdaf6ccf3fcc96019a340839d
BoleOle <ole_marius_j@hotmail.com>
2022-08-31 10:20:13 +0200

    added grid_traveler_tab2.py that can handle all kinds of input sizes

---
+++ grid_traveler/grid_traveler_tab2.py
@@ -0,0 +1,27 @@
+tabell = []
+
+def grid_traveler(m, n):
+    for i in range(m + 1):
+        tabell.append([])
+        for j in range(n + 1):
+            tabell[i].append(0)
+    tabell[1][1] = 1
+    for i in range(len(tabell)):
+        if i != len(tabell) - 1:
+            for j in range(0, len(tabell[i]), 1):
+                if tabell[i][j] != 0 and j != len(tabell[i]) - 1:
+                    tabell[i][j + 1] += tabell[i][j]
+                    tabell[i + 1][j] += tabell[i][j]
+                elif tabell[i][j] != 0 and j == len(tabell[i]) - 1:
+                    tabell[i + 1][j] += tabell[i][j]
+        else:
+            for j in range(0, len(tabell[i]), 1):
+                if tabell[i][j] != 0 and j != len(tabell[i]) - 1:
+                    tabell[i][j + 1] += tabell[i][j]
+            return tabell[i][j]
+        
+
+
+
+print(str(grid_traveler(4,3)))
+print(tabell)
